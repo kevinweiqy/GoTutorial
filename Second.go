11a0aa658ec7bf2448b53113e754f554a2b908a6
kevin <kevinweiqy@gmail.com>
2017-02-19 15:51:44 +0800

    Initial commit

---
+++ Second.go
@@ -0,0 +1,118 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*fmt.Println("hellomojo")
+	fmt.Println("just do it")
+	var a int = 9
+	fmt.Println(a)*/
+	/*x:=3
+	fmt.Println(x)*/
+	/*fmt.Println("Hello, world")
+	fmt.Println("I'm in Go Progrmming World")*/
+	/*
+		var a1 bool = true
+		fmt.Println(a1)
+
+		var b2 bool = false
+		fmt.Println(b2)
+
+
+		var c3 uint64
+		fmt.Println(c3)
+
+		var c4 int32
+		fmt.Println(c4)*/
+
+	//define float variables
+	/*var f1 float32
+	fmt.Println(f1)*/
+
+	/*var a, b, c float64;
+	fmt.Println("a =", a, ", b = ", b, ", c = " , c)*/
+
+	/*var a float64
+	a = 20.0
+	fmt.Println(a)
+	fmt.Printf("a is the type of %T\n", a)*/
+
+	/*var x float64 = 20.0
+	y := 39
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("x is the type of %T\n", x)
+	fmt.Printf("y is the type of %T\n", y)*/
+
+	/*var a, b, c = 3, 2.9, "foo"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("a is the of %T\n", a)
+	fmt.Printf("b is the type of %T\n", b)
+	fmt.Printf("c is the type of %T\n", c)*/
+
+	/*var x float64;
+	x = 23L;
+	fmt.Println(x)*/
+
+	/*fmt.Println("Hello\tWorld")*/
+
+	/*var s = "hello, dear"
+
+	fmt.Println(s)*/
+
+	/*const HEIGHT int = 10
+	fmt.Println(HEIGHT)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	const HEIGHT int =  20
+	const WIDTH int  = 13
+	var AREA int
+	AREA = HEIGHT * WIDTH
+	fmt.Printf("The area is %d", AREA)
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
